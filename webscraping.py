db4fd025927de47e5fa3b0ab425e7e9461ca5f63
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-09-26 20:58:26 -0400

    extrated links

--- webscraping.py
+++ webscraping.py
@@ -2,25 +2,40 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-# System (command line arguments)
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
-
 
 
 def main():
     # loops through arguments from command line
     for x in range(1, len(sys.argv)):
         # requests (Gets website url content)
-        request = requests.get("https://en.wikipedia.org/wiki/" + str(sys.argv[x]) + "_(disambiguation)").content
+        try:
+            # gets website url content
+            request = requests.get("https://en.wikipedia.org/wiki/{}".format(sys.argv[x])).content
 
-        # BeautifulSoup
-        soup = BeautifulSoup(request, "lxml")
+            # If website is invalid 
+            if "Wikipedia does not have an article with this exact name" in str(request):
+                print("Wikipedia does not have an article with this exact name")
+                exit()
 
-        # loops through all text in the website
-        for text in soup.find_all():
-            if str(sys.argv[x]) in text.get_text():
-                print(text.get_text())
+            # BeautifulSoup
+            soup = BeautifulSoup(request, "lxml")
+            print(soup.get_text())
+            # If the response is valid it should then load beautifulsoup and search the page for the text of the results
+            links = []
+            words = []
+
+
+            divTag = soup.find_all("div", {"id": "mw-content-text"})
+
+            for tag in divTag:
+                link = tag.find_all("a")
+                for tag in tdTags:
+                    print tag.text
+
+        except Exception as e:
+            print(e)
+            print("failed to get website url at https://en.wikipedia.org/wiki/{}".format(sys.argv[x]))
+            exit()
 
     return 0
 
